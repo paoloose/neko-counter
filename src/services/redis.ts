2253293ab1fa9714b16f5d7a13fb13e062f6001b
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-04 03:36:28 -0500

    feat(db): add redis client

---
+++ src/services/redis.ts
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: process.env['REDIS_URL']
+});
+
+client.on('error', () => {
+  console.error('Failed to connect to redis');
+  console.error('Exiting.');
+  process.exit(1);
+});
+
+client.on('ready', () => {
+  console.log('Connected to redis');
+});
+
+await client.connect();
+
+export const redis = client;
