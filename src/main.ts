652ee2d018af145f8f7f8b4a4fcbbfda7aff35d9
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-04 03:36:41 -0500

    feat: setup main endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,49 @@
 import express from 'express'
 import { generateNekoBanner } from './banner';
+import { redis } from './services/redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-
-const DB: Record<string, number> = {
-};
+const PORT = process.env['PORT'] || 3000;
 
 app.get('/counter', async (req, res) => {
   const { id } = req.query;
 
   if (typeof id !== 'string') return res.end(400);
 
-  if (DB[id] === undefined) {
-    DB[id] = 0;
-  }
-  const count = ++DB[id];
+  const neko_raw_info = await redis.get(id);
+  let neko_counter_info: CounterInfo | null = null;
 
-  const buffer = await generateNekoBanner(count);
+  try {
+    neko_counter_info = JSON.parse(neko_raw_info!) as CounterInfo;
+    neko_counter_info.count++;
+  }
+  catch {
+    neko_counter_info = {
+      id,
+      count: 1,
+      created_at: Date.now()
+    };
+  }
+
+  const buffer = await generateNekoBanner(neko_counter_info.count);
+
   res.writeHead(200, {
     'content-type': 'image/png',
     'content-length': buffer.length
   });
-  res.end(buffer);
+
+  try {
+    await redis.set(id, JSON.stringify(neko_counter_info));
+  } catch {
+    console.error('Failed to update counter');
+  }
+
+  return res.end(buffer);
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log('Listening for neko requests');
 });
