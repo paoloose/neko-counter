9d09d7d1b914b457ea3883b429e6611b17731d03
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-04 04:17:18 -0500

    fix: fix directories not created

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,5 +1,9 @@
 import { join } from 'node:path';
+import { mkdir } from 'node:fs/promises';
 
 export const BASE_DIR = process.cwd();
 export const CACHE_DIR = join(BASE_DIR, 'cache');
 export const FONTS_DIR = join(BASE_DIR, 'fonts');
+
+mkdir(CACHE_DIR, { recursive: true }).catch(console.error);
+mkdir(FONTS_DIR, { recursive: true }).catch(console.error);
