274f64c682309a33c13a9b86fdcc7e97aefa9468
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-04 23:59:27 -0500

    feat(cors): allow POST method

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 
 export function cors(_req: Request, res: Response, next: NextFunction) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   next();
 }
