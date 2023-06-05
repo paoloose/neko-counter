a2903ce265f553c6d0624a261cf0ccc6452364db
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-05 03:25:53 -0500

    remove unnecessary logs

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,14 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function cors(_req: Request, res: Response, next: NextFunction) {
-  // Print request information
-  console.log({
-    method: _req.method,
-    url: _req.url,
-    headers: _req.headers,
-    ip: _req.ip
-  });
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   next();
