49eead36cff7b6b43df4d06fc378311ab3811d24
Paolo Flores <pflores.fisi22@gmail.com>
2023-06-05 05:20:31 -0500

    feat: allow creating profiles via GET

--- src/routes/create.ts
+++ src/routes/create.ts
@@ -3,6 +3,48 @@ import { NekoUser } from '../schemas/NekoUser';
 
 const router = Router();
 
+router.get('/create', async (req, res) => {
+  const payload = req.query;
+
+  if (!isValidPayload(payload)) {
+    return res.status(400).send({
+      status: 400,
+      error: 'How bad! It seems like the content is a bit nyot to my wiking! Pwease, oh pwease, take a peek at the docs over hewe: https://github.com/paowoowoo/neko-countew',
+      payload: payload || 10
+    });
+  }
+
+  const registered = await NekoUser.exists(payload.id);
+  if (registered) {
+    return res.status(400).send({
+      status: 400,
+      error: 'Oh noesie-wosies! It seems like you\'ve already cwated a pwofile! Pwease, oh pwease, use a diffewent id! (´；ω；｀)'
+    });
+  }
+
+  const new_profile = new NekoUser({
+    id: payload.id,
+    password: payload.password,
+    count: 0,
+    github_only: payload.github_only ?? true
+  });
+
+  try {
+    await new_profile.save();
+  }
+  catch {
+    return res.status(500).send({
+      status: 500,
+      error: 'Something went wrong... so sowwy! (´；ω；｀)'
+    });
+  }
+
+  res.send({
+    status: 200,
+    message: 'Yay! You\'ve created a new neko profile! Now you can use it to count your profie view!'
+  });
+});
+
 router.post('/create', async (req, res) => {
   const payload = req.body;
 
@@ -25,7 +67,7 @@ router.post('/create', async (req, res) => {
   if (registered) {
     return res.status(400).send({
       status: 400,
-      error: 'Oh noesie-wosies! It seems like you\'ve already cwated a pwofile! Pwease, oh pwease, use a diffewent id!'
+      error: 'Oh noesie-wosies! It seems like you\'ve already cwated a pwofile! Pwease, oh pwease, use a diffewent id! (´；ω；｀)'
     });
   }
 
